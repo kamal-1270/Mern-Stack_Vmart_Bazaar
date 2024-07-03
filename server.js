619f5d73a465cf92967dd42755af4d4cad02da68
kamal kumar singh <147371118+kamal-1270@users.noreply.github.com>
2024-07-04 03:40:49 +0530

    changes

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ import authRoutes from './routes/authRoute.js'
 import cors from 'cors'
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
-// import path from 'path'
+import path from 'path'
+import { fileURLToPath } from 'url';
 //configure env
 dotenv.config();
 
 //database config
 connectDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //rest object
 const app=express();
 
@@ -19,6 +24,7 @@ const app=express();
 app.use(cors())
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname,"./client/build")))
 
 // routes
 app.use("/auth",authRoutes);
@@ -26,10 +32,15 @@ app.use('/category',categoryRoutes)
 app.use('/product',productRoutes)
 
 
-app.get("/",(req,res)=>{
-    res.send(
-        "<h1>Welcome the ecomerce app</h1>"
-    )
+// app.get("/",(req,res)=>{
+//     res.send(
+//         "<h1>Welcome the ecomerce app</h1>"
+//     )
+// })
+
+//rest api
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
 })
 
 //port
